57dd58b749250816201fe9ac266964b37c432ee8
Jun Hao, Garren <teejunhao@gmail.com>
2023-11-13 01:26:25 +0800

    working questions selection

--- streamlit-app/pages/Milestone 2.py
+++ streamlit-app/pages/Milestone 2.py
@@ -104,19 +104,260 @@ def evaluate(test, pred):
 evaluate( y_test, y_pred_rf )
 # ______________________________________Creating Questionaire ______________________________________
 
+
+
+###############
+data = [
+'Age',
+ 'Coding Experience (in years)',
+ 'Education level_attainedOrGGtoAttain',
+ 'Gender - Selected Choice',
+ 'Job_JobScope - Analyze and understand data to influence product or business decisions',
+ 'Job_JobScope - Build and/or run a machine learning service that operationally improves my product or workflows',
+ 'Job_JobScope - Build and/or run the data infrastructure that my business uses for storing, analyzing, and operationalizing data',
+ 'Job_JobScope - Build prototypes to explore applying machine learning to new areas',
+ 'Job_JobScope - Do research that advances the state of the art of machine learning',
+ 'Job_JobScope - Experimentation and iteration to improve existing ML models',
+ 'Job_Salary',
+ 'Learning platforms tried - How well known are the platforms (platforms with good marketing) - Cloud-certification programs (direct from AWS, Azure, GCP, or similar)',
+ 'Learning platforms tried - How well known are the platforms (platforms with good marketing) - Coursera',
+ 'Learning platforms tried - How well known are the platforms (platforms with good marketing) - DataCamp',
+ 'Learning platforms tried - How well known are the platforms (platforms with good marketing) - Fast.ai',
+ 'Learning platforms tried - How well known are the platforms (platforms with good marketing) - Kaggle Learn Courses',
+ 'Learning platforms tried - How well known are the platforms (platforms with good marketing) - LinkedIn Learning',
+ 'Learning platforms tried - How well known are the platforms (platforms with good marketing) - Udacity',
+ 'Learning platforms tried - How well known are the platforms (platforms with good marketing) - Udemy',
+ 'Learning platforms tried - How well known are the platforms (platforms with good marketing) - University Courses (resulting in a university degree)',
+ 'Learning platforms tried - How well known are the platforms (platforms with good marketing) - edX',
+ 'Popular BI tool brands - Alteryx',
+ 'Popular BI tool brands - Amazon QuickSight',
+ 'Popular BI tool brands - Domo',
+ 'Popular BI tool brands - Einstein Analytics',
+ 'Popular BI tool brands - Google Data Studio',
+ 'Popular BI tool brands - Looker',
+ 'Popular BI tool brands - Microsoft Azure Synapse',
+ 'Popular BI tool brands - Microsoft Power BI',
+ 'Popular BI tool brands - Qlik',
+ 'Popular BI tool brands - Qlik Sense',
+ 'Popular BI tool brands - SAP Analytics Cloud',
+ 'Popular BI tool brands - Salesforce',
+ 'Popular BI tool brands - Sisense',
+ 'Popular BI tool brands - TIBCO Spotfire',
+ 'Popular BI tool brands - Tableau',
+ 'Popular BI tool brands - Tableau CRM',
+ 'Popular BI tool brands - Thoughtspot',
+ 'Popular Cloud Computing Platform Brand - Alibaba Cloud',
+ 'Popular Cloud Computing Platform Brand - Amazon Web Services (AWS)',
+ 'Popular Cloud Computing Platform Brand - Google Cloud Platform (GCP)',
+ 'Popular Cloud Computing Platform Brand - Huawei Cloud',
+ 'Popular Cloud Computing Platform Brand - IBM Cloud / Red Hat',
+ 'Popular Cloud Computing Platform Brand - Microsoft Azure',
+ 'Popular Cloud Computing Platform Brand - Oracle Cloud',
+ 'Popular Cloud Computing Platform Brand - SAP Cloud',
+ 'Popular Cloud Computing Platform Brand - Salesforce Cloud',
+ 'Popular Cloud Computing Platform Brand - Tencent Cloud',
+ 'Popular Cloud Computing Platform Brand - VMware Cloud',
+ 'Popular Cloud Computing Product Brand - AWS Lambda',
+ 'Popular Cloud Computing Product Brand - Amazon EC2',
+ 'Popular Cloud Computing Product Brand - Amazon Elastic Compute Cloud (EC2)',
+ 'Popular Cloud Computing Product Brand - Amazon Elastic Container Service',
+ 'Popular Cloud Computing Product Brand - Azure Cloud Services',
+ 'Popular Cloud Computing Product Brand - Azure Functions',
+ 'Popular Cloud Computing Product Brand - Google Cloud App Engine',
+ 'Popular Cloud Computing Product Brand - Google Cloud Compute Engine',
+ 'Popular Cloud Computing Product Brand - Google Cloud Functions',
+ 'Popular Cloud Computing Product Brand - Google Cloud Run',
+ 'Popular Cloud Computing Product Brand - Microsoft Azure Container Instances',
+ 'Popular Cloud Computing Product Brand - Microsoft Azure Virtual Machines',
+ 'Popular Computer Vision Methods - General purpose image/video tools (PIL, cv2, skimage, etc)',
+ 'Popular Computer Vision Methods - Generative Networks (GAN, VAE, etc)',
+ 'Popular Computer Vision Methods - Image classification and other general purpose networks (VGG, Inception, ResNet, ResNeXt, NASNet, EfficientNet, etc)',
+ 'Popular Computer Vision Methods - Image segmentation methods (U-Net, Mask R-CNN, etc)',
+ 'Popular Computer Vision Methods - Object detection methods (YOLOv3, RetinaNet, etc)',
+ 'Popular Computer Vision Methods - Object detection methods (YOLOv6, RetinaNet, etc)',
+ 'Popular Computer Vision Methods - Vision transformer networks (ViT, DeiT, BiT, BEiT, Swin, etc)',
+ 'Popular ML Algorithms - Autoencoder Networks (DAE, VAE, etc)',
+ 'Popular ML Algorithms - Bayesian Approaches',
+ 'Popular ML Algorithms - Convolutional Neural Networks',
+ 'Popular ML Algorithms - Decision Trees or Random Forests',
+ 'Popular ML Algorithms - Dense Neural Networks (MLPs, etc)',
+ 'Popular ML Algorithms - Evolutionary Approaches',
+ 'Popular ML Algorithms - Generative Adversarial Networks',
+ 'Popular ML Algorithms - Gradient Boosting Machines (xgboost, lightgbm, etc)',
+ 'Popular ML Algorithms - Graph Neural Networks',
+ 'Popular ML Algorithms - Linear or Logistic Regression',
+ 'Popular ML Algorithms - Recurrent Neural Networks',
+ 'Popular ML Algorithms - Transformer Networks (BERT, gpt-3, etc)',
+ 'Popular ML frameworks - Caret',
+ 'Popular ML frameworks - CatBoost',
+ 'Popular ML frameworks - Fast.ai',
+ 'Popular ML frameworks - H2O 3',
+ 'Popular ML frameworks - Huggingface',
+ 'Popular ML frameworks - JAX',
+ 'Popular ML frameworks - Keras',
+ 'Popular ML frameworks - LightGBM',
+ 'Popular ML frameworks - MXNet',
+ 'Popular ML frameworks - Prophet',
+ 'Popular ML frameworks - PyTorch',
+ 'Popular ML frameworks - PyTorch Lightning',
+ 'Popular ML frameworks - Scikit-learn',
+ 'Popular ML frameworks - TensorFlow',
+ 'Popular ML frameworks - Tidymodels',
+ 'Popular ML frameworks - Xgboost',
+ 'Popular ML product brand - Alteryx',
+ 'Popular ML product brand - Amazon Forecast',
+ 'Popular ML product brand - Amazon Rekognition',
+ 'Popular ML product brand - Amazon SageMaker',
+ 'Popular ML product brand - Azure Cognitive Services',
+ 'Popular ML product brand - Azure Machine Learning Studio',
+ 'Popular ML product brand - C3.ai',
+ 'Popular ML product brand - DataRobot',
+ 'Popular ML product brand - Databricks',
+ 'Popular ML product brand - Dataiku',
+ 'Popular ML product brand - Domino Data Lab',
+ 'Popular ML product brand - Google Cloud AI Platform / Google Cloud ML Engine',
+ 'Popular ML product brand - Google Cloud Natural Language',
+ 'Popular ML product brand - Google Cloud Vertex AI',
+ 'Popular ML product brand - Google Cloud Video AI',
+ 'Popular ML product brand - Google Cloud Vision AI',
+ 'Popular ML product brand - H2O AI Cloud',
+ 'Popular ML product brand - Rapidminer',
+ 'Popular NLP Methods - Contextualized embeddings (ELMo, CoVe)',
+ 'Popular NLP Methods - Encoder-decoder models (seq2seq, vanilla transformers)',
+ 'Popular NLP Methods - Encoder-decorder models (seq2seq, vanilla transformers)',
+ 'Popular NLP Methods - Transformer language models (GPT-3, BERT, XLnet, etc)',
+ 'Popular NLP Methods - Word embeddings/vectors (GLoVe, fastText, word2vec)',
+ 'Popular auto ML product brand - Amazon Sagemaker Autopilot',
+ 'Popular auto ML product brand - Auto-Keras',
+ 'Popular auto ML product brand - Auto-Sklearn',
+ 'Popular auto ML product brand - Auto_ml',
+ 'Popular auto ML product brand - Azure Automated Machine Learning',
+ 'Popular auto ML product brand - DataRobot AutoML',
+ 'Popular auto ML product brand - Databricks AutoML',
+ 'Popular auto ML product brand - Google Cloud AutoML',
+ 'Popular auto ML product brand - H20 Driverless AI',
+ 'Popular auto ML product brand - H2O Driverless AI',
+ 'Popular auto ML product brand - MLbox',
+ 'Popular auto ML product brand - Tpot',
+ 'Popular auto ML product brand - Xcessiv',
+ 'Popular data product brands used (Databases, Warehouses, Lakes) - Amazon Athena',
+ 'Popular data product brands used (Databases, Warehouses, Lakes) - Amazon Aurora',
+ 'Popular data product brands used (Databases, Warehouses, Lakes) - Amazon DynamoDB',
+ 'Popular data product brands used (Databases, Warehouses, Lakes) - Amazon RDS',
+ 'Popular data product brands used (Databases, Warehouses, Lakes) - Amazon Redshift',
+ 'Popular data product brands used (Databases, Warehouses, Lakes) - Google Cloud BigQuery',
+ 'Popular data product brands used (Databases, Warehouses, Lakes) - Google Cloud BigTable',
+ 'Popular data product brands used (Databases, Warehouses, Lakes) - Google Cloud Firestore',
+ 'Popular data product brands used (Databases, Warehouses, Lakes) - Google Cloud SQL',
+ 'Popular data product brands used (Databases, Warehouses, Lakes) - Google Cloud Spanner',
+ 'Popular data product brands used (Databases, Warehouses, Lakes) - IBM Db2',
+ 'Popular data product brands used (Databases, Warehouses, Lakes) - Microsoft Access',
+ 'Popular data product brands used (Databases, Warehouses, Lakes) - Microsoft Azure Cosmos DB',
+ 'Popular data product brands used (Databases, Warehouses, Lakes) - Microsoft Azure Data Lake Storage',
+ 'Popular data product brands used (Databases, Warehouses, Lakes) - Microsoft Azure SQL Database',
+ 'Popular data product brands used (Databases, Warehouses, Lakes) - Microsoft SQL Server',
+ 'Popular data product brands used (Databases, Warehouses, Lakes) - MongoDB',
+ 'Popular data product brands used (Databases, Warehouses, Lakes) - MySQL',
+ 'Popular data product brands used (Databases, Warehouses, Lakes) - Oracle Database',
+ 'Popular data product brands used (Databases, Warehouses, Lakes) - PostgreSQL',
+ 'Popular data product brands used (Databases, Warehouses, Lakes) - PostgresSQL',
+ 'Popular data product brands used (Databases, Warehouses, Lakes) - SQLite',
+ 'Popular data product brands used (Databases, Warehouses, Lakes) - Snowflake',
+ 'Popular hosted notebook products - Amazon EMR Notebooks',
+ 'Popular hosted notebook products - Amazon Sagemaker Studio',
+ 'Popular hosted notebook products - Amazon Sagemaker Studio Lab',
+ 'Popular hosted notebook products - Amazon Sagemaker Studio Notebooks',
+ 'Popular hosted notebook products - Azure Notebooks',
+ 'Popular hosted notebook products - Binder / JupyterHub',
+ 'Popular hosted notebook products - Code Ocean',
+ 'Popular hosted notebook products - Colab Notebooks',
+ 'Popular hosted notebook products - Databricks Collaborative Notebooks',
+ 'Popular hosted notebook products - Deepnote Notebooks',
+ 'Popular hosted notebook products - Google Cloud AI Platform Notebooks',
+ 'Popular hosted notebook products - Google Cloud Datalab',
+ 'Popular hosted notebook products - Google Cloud Datalab Notebooks',
+ 'Popular hosted notebook products - Google Cloud Notebooks (AI Platform / Vertex AI)',
+ 'Popular hosted notebook products - Google Cloud Vertex AI Workbench',
+ 'Popular hosted notebook products - Gradient Notebooks',
+ 'Popular hosted notebook products - Hex Workspaces',
+ 'Popular hosted notebook products - IBM Watson Studio',
+ 'Popular hosted notebook products - Kaggle Notebooks',
+ 'Popular hosted notebook products - Noteable Notebooks',
+ 'Popular hosted notebook products - Observable Notebooks',
+ 'Popular hosted notebook products - Paperspace / Gradient',
+ 'Popular hosted notebook products - Zeppelin / Zepl Notebooks',
+ 'Popular media sources for Data Science - Blogs (Towards Data Science, Analytics Vidhya, etc)',
+ 'Popular media sources for Data Science - Course Forums (forums.fast.ai, Coursera forums, etc)',
+ "Popular media sources for Data Science - Email newsletters (Data Elixir, O'Reilly Data & AI, etc)",
+ 'Popular media sources for Data Science - Journal Publications (peer-reviewed journals, conference proceedings, etc)',
+ 'Popular media sources for Data Science - Kaggle (notebooks, forums, etc)',
+ 'Popular media sources for Data Science - Podcasts (Chai Time Data Science, Oâ€™Reilly Data Show, etc)',
+ 'Popular media sources for Data Science - Podcasts (Chai Time Data Science, O’Reilly Data Show, etc)',
+ 'Popular media sources for Data Science - Reddit (r/machinelearning, etc)',
+ 'Popular media sources for Data Science - Slack Communities (ods.ai, kagglenoobs, etc)',
+ 'Popular media sources for Data Science - Twitter (data science influencers)',
+ 'Popular media sources for Data Science - YouTube (Kaggle YouTube, Cloud AI Adventures, etc)',
+ 'Popular programming language - Bash',
+ 'Popular programming language - C',
+ 'Popular programming language - C#',
+ 'Popular programming language - C++',
+ 'Popular programming language - Go',
+ 'Popular programming language - Java',
+ 'Popular programming language - Javascript',
+ 'Popular programming language - Julia',
+ 'Popular programming language - MATLAB',
+ 'Popular programming language - PHP',
+ 'Popular programming language - Python',
+ 'Popular programming language - R',
+ 'Popular programming language - SQL',
+ 'Popular programming language - Swift',
+ 'Popular tools to monitor ML/Experiments - Aporia',
+ 'Popular tools to monitor ML/Experiments - Arize',
+ 'Popular tools to monitor ML/Experiments - ClearML',
+ 'Popular tools to monitor ML/Experiments - Comet.ml',
+ 'Popular tools to monitor ML/Experiments - DVC',
+ 'Popular tools to monitor ML/Experiments - Domino Model Monitor',
+ 'Popular tools to monitor ML/Experiments - Evidently AI',
+ 'Popular tools to monitor ML/Experiments - Fiddler',
+ 'Popular tools to monitor ML/Experiments - Guild.ai',
+ 'Popular tools to monitor ML/Experiments - MLflow',
+ 'Popular tools to monitor ML/Experiments - Neptune.ai',
+ 'Popular tools to monitor ML/Experiments - Polyaxon',
+ 'Popular tools to monitor ML/Experiments - Sacred + Omniboard',
+ 'Popular tools to monitor ML/Experiments - TensorBoard',
+ 'Popular tools to monitor ML/Experiments - Trains',
+ 'Popular tools to monitor ML/Experiments - Weights & Biases',
+ 'Popular tools to monitor ML/Experiments - WhyLabs',
+ 'Years in ML']
+########
+
+questions,answers = [],[]
+for line in data:
+    words = line.split(" ")
+    if "-" in words:
+        idx = [index for index, value in enumerate(words) if value == "-"][-1] # get last index of -
+        questions.append(" ".join(words[:idx]))
+        
+        answers.append(" ".join(words[idx+1:]))
+    else:
+        questions.append(line)
+        answers.append("None")
+questions_answers = {}
+# Iterate through the lists and populate the dictionary
+for key, value in zip(questions, answers):
+    if key in questions_answers:
+        questions_answers[key].append(value)
+    else:
+        questions_answers[key] = [value]
+
+##############
+
 def create_questionnaire(questions_answers):
     for question, answer_options in questions_answers.items():
         selected_answer = st.selectbox(question, answer_options)
         st.write(f"You selected for '{question}': {selected_answer}")
         st.write("---")  # Add a separator between questions
 
-# Define your questions and answers
-questions_answers = {
-    "What is your name?": ["Alice", "Bob", "Charlie"],
-    "How old are you?": ["Under 18", "18-30", "31-50", "Over 50"],
-    "What is your favorite color?": ["Red", "Blue", "Green", "Other"],
-    # Add more questions and answers as needed
-}
 
 # Call the function to create the questionnaire
 create_questionnaire(questions_answers)
@@ -130,13 +371,14 @@ def get_recommendations(model, userInputs):
     """return the model prediction based on user input"""
     return model.predict(userInputs)
     
-
-
-
+@st.cache
+def encodeUserInput(userinput):
+    """encode the user data to be passed into the random forest model """
+    return 
 # _________________________ Streamlit UI ____________________________
 st.title("Job Recommender System")
 
 if st.button("Get Recommendations"):
-    recommendations = get_recommendations(model = rnd_clf, userInputs=user_input())
+    #recommendations = get_recommendations(model = rnd_clf, userInputs=encodeUserInput(user_input())) 
     st.write("Recommended Jobs based on your profile:")
     st.write(recommendations)
